3a749e0e04c223bbf289fd84e20e3b548f77f06a
Lekuba Ntoane <l.ntoane@gmail.com>
2022-06-29 11:00:09 +0200

    Project restructured

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import logo from './react-logo.png';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
-function App() {
-  return (
-    <div>
-      <img src={logo} width="40px" />
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    );
 }
-
-export default App;
